55715864f94b5750159d11acb0858fab87b1ceba
haminhduc <haminhduc407@gmail.com>
2023-03-20 12:38:21 -0400

    add stop

--- script.js
+++ script.js
@@ -1,35 +1,52 @@
 const startButton = document.querySelector("#start-button");
 const stopButton = document.querySelector("#stop-button");
 
-const isTurnedOn = false;
+let isWorking = false;
+let n = 0;
 
-function turnGreen(cornerNumber) {
-  const selectedCorner = "corner-" + cornerNumber;
-  const turn_green = document.getElementById(selectedCorner);
-  console.log(turn_green);
-  turn_green.classList.add("pop-up");
+function letRun() {
+  isWorking = true;
+  setInterval(() => {
+    if (isWorking === true) {
+      const random = Math.floor(Math.random() * 5 + 1);
+      // console.log(random);
+      const selectedCorner = "corner-" + random;
+      const turn_green = document.getElementById(selectedCorner);
+      // console.log(turn_green);
+      turn_green.classList.add("pop-up");
+
+      setTimeout(
+        () => {
+          turnOff(random);
+        },
+        1000,
+        random
+      );
+    } else return;
+  }, 3000);
 }
 
 function turnOff(cornerNumber) {
   const selectedCorner = "corner-" + cornerNumber;
   const turn_off = document.getElementById(selectedCorner);
-  console.log(turn_off);
+  // console.log(turn_off);
   turn_off.classList.remove("pop-up");
 }
 
-startButton.addEventListener("click", () => {
-  setInterval(() => {
-    const random = Math.floor(Math.random() * 5 + 1);
-    // console.log(random);
-    turnGreen(random);
+startButton.addEventListener("click", letRun);
 
-    setTimeout(
-      () => {
-        // console.log(random);
-        turnOff(random);
-      },
-      1000,
-      random
-    );
-  }, 2500);
+stopButton.addEventListener("click", () => {
+  // console.log("stop pls");
+
+  isWorking = false;
 });
+// document.addEventListener function (e) {
+//   // if (e.key == " " || e.key == "Space" || e.key == 32) {
+//   //   if (isWorking === false) {
+//   //     isWorking = true;
+//   //   } else {
+//   //     isWorking = false;
+//   //   }
+//   // }
+//   console("space pressed");
+// };
