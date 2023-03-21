9a5c9ed7a8a39e7a8babe871ce28f29203f1bd4f
haminhduc <haminhduc407@gmail.com>
2023-03-21 01:31:53 -0400

    move letRun to under turnOff function

--- script.js
+++ script.js
@@ -4,10 +4,29 @@ const container = document.querySelector(".container");
 let isWorking = false;
 let n = 0;
 
+function genRandAndShow(isWorking) {
+  if (isWorking === true) {
+    const random = Math.floor(Math.random() * 5 + 1);
+    // console.log(random);
+    const selectedCorner = "corner-" + random;
+    const turn_green = document.getElementById(selectedCorner);
+    // console.log(turn_green);
+    turn_green.classList.add("pop-up");
+    return random;
+  } else return 0;
+}
+
+function turnOff(cornerNumber) {
+  const selectedCorner = "corner-" + cornerNumber;
+  const turn_off = document.getElementById(selectedCorner);
+  // console.log(turn_off);
+  turn_off.classList.remove("pop-up");
+}
+
 function letRun() {
   isWorking = true;
   var id = setInterval(() => {
-  var on = genRandAndShow(isWorking)
+    var on = genRandAndShow(isWorking);
     setTimeout(
       () => {
         turnOff(on);
@@ -17,25 +36,6 @@ function letRun() {
     );
   }, 3000);
   container.dataset.interval = id;
-}
-
-function genRandAndShow(isWorking){
-  if (isWorking === true) {
-    const random = Math.floor(Math.random() * 5 + 1);
-    // console.log(random);
-    const selectedCorner = "corner-" + random;
-    const turn_green = document.getElementById(selectedCorner);
-    // console.log(turn_green);
-    turn_green.classList.add("pop-up");
-    return random
-  } else return 0;
-}
-
-function turnOff(cornerNumber) {
-  const selectedCorner = "corner-" + cornerNumber;
-  const turn_off = document.getElementById(selectedCorner);
-  // console.log(turn_off);
-  turn_off.classList.remove("pop-up");
 }
 
 startButton.addEventListener("click", letRun);
